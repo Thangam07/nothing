769e4f7ddda9d49f2316b3bf91d92217a8cebdc8
Thangam07 <thangams009@gmail.com>
2023-03-30 13:06:30 +0530

    arrlist

---
+++ arrlist.java.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+  public class ArrList1 {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Alice");
+        names.add("Bob");
+        names.add("Charlie");
+        names.add(null);
+        System.out.println(names);
+
+        Iterator<String> iterator = names.iterator();
+        while (iterator.hasNext()) {
+            String name = iterator.next();
+            System.out.println(name);
+        }
+    }
+}
